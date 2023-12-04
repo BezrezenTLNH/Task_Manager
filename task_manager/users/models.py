8793edfd058071db6e2d4097c1075512db68979b
Mikhail Zherebtsov <bezrezen97@gmail.com>
2023-12-04 23:14:43 +0100

    change users page

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Person(User):
+
+    class Meta:
+        proxy = True
+
+    def __str__(self):
+        return self.get_full_name()
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
+from django.views import View
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def index(request):
     return render(request, 'users/index.html')
+
+class IndexUsersView(ListView):
+
+    template_name = 'users/users.html'
+    model = User
+    context_object_name = 'users'
+
+# @require_http_methods(['GET', 'POST'])
+# def get(request):
