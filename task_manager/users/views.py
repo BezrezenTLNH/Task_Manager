5a874c63b401f162b1fd37f62897885985281e9f
Mikhail Zherebtsov <bezrezen97@gmail.com>
2023-12-08 19:21:23 +0100

    add create user form

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from django.views import View
+from django.contrib import messages
 from django.views.generic import CreateView, UpdateView, DeleteView, ListView
 from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.utils.translation import gettext as _
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 
@@ -19,9 +21,16 @@ class UserFormCreateView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('users')
 
+    def form_valid(self, form):
+        messages.success(
+            self.request, _('Your profile has been successfully created!')
+        )
+        return super().form_valid(form)
+
 class UserFormChangeView(UpdateView):
     template_name = 'users/update.html'
     form_class = CustomUserChangeForm
+
 
 class UserFormDeleteView(DeleteView):
     model = User
