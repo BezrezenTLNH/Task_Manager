ff3a6bb37f1d2d93964ef7947ee23f3135b86b96
BezrezenTLNH <bezrezen97@gmail.com>
2023-12-13 15:44:31 +0100

    complete statuses app

--- task_manager/statuses/admin.py
+++ task_manager/statuses/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
 
-# Register your models here.
+from .models import StatusModel
+
+
+@admin.register(StatusModel)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('name',
+                    'created_at')
+    search_fields = ['name', 'created_at']
+    list_filter = (('created_at', DateFieldListFilter),)
